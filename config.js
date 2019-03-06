9a29a6d74e6c96806921539a552a4d3ae7df83e5
Luis Miguel del Corral Cuervo <luisdelcorral@corporativo.adams.es>
2019-03-06 13:46:37 +0100

    Commit

---
+++ config.js
@@ -0,0 +1,9 @@
+// in your terminal, write > cp config.default.js config.js and change the following values
+// get these keys in https://apps.twitter.com/app/new 
+// change
+module.exports = {
+  consumer_key: 'wtGFRArchJwdQXmAnO6g',
+  consumer_secret: 'KjzYPCKAWqkGXh6AMQt3M06dBinAoMuEWMK4ZR6IZA',
+  access_token_key: '2338203222-ju5QrSjIfxuUzuwifWaTlveoMvj3AxxPOfXSYR9',
+  access_token_secret: 'mcL0bTgV6coLia4IxM5gEQNSwJyDWEY69XQhLdpKODW3I'
+}
